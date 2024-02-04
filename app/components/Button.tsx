cc49a1d518b0908528f5868b229633a3547bdd06
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-02-04 23:10:30 +0300

    created the hero section

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 interface Props {
   type: "submit" | "button";
   title: string;
-  icon: string;
+  icon?: string;
   variant:
     | "btn_white_text"
     | "btn_green"
@@ -15,7 +15,7 @@ interface Props {
 const Button = ({ type, title, icon, variant }: Props) => {
   return (
     <button className={`flexCenter gap-3 rounded-full ${variant}`} type={type}>
-      <Image src={icon} alt={title} width={24} height={24} />
+      <Image src={icon!} alt={title} width={24} height={24} />
       {title}
     </button>
   );
